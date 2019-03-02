dc5cb42e376600a0357949dff04928203e9a22a1
v.nedashkovskiy <v.nedashkovskiy@corp.mail.ru>
2019-03-03 01:33:51 +0300

    add: addition to the previous commit

--- src/com/texnedo/ShortestDistanceToCharacter.java
+++ src/com/texnedo/ShortestDistanceToCharacter.java
@@ -1,4 +1,42 @@
 package com.texnedo;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class ShortestDistanceToCharacter {
+
+    public static void main(String[] args) {
+        ShortestDistanceToCharacter distance = new ShortestDistanceToCharacter();
+        System.out.println(Arrays.toString(distance.shortestToChar("loveleetcode", 'e')));
+    }
+
+    public int[] shortestToChar(String S, char C) {
+        int[] result = new int[S.length()];
+        LinkedList<Integer> positions = new LinkedList<>();
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == C) {
+                positions.offer(i);
+            }
+        }
+        for (int i = 0; i < S.length(); i++) {
+            Integer pos = positions.peekFirst();
+            if (i < pos) {
+                result[i] = pos - i;
+            } else if (positions.size() > 1) {
+                int leftPos = positions.pollFirst();
+                int rightPos = positions.peekFirst();
+                if (Math.abs(i - leftPos) < Math.abs(i - rightPos)) {
+                    positions.offerFirst(leftPos);
+                    result[i] = i - leftPos;
+                } else {
+                    result[i] = rightPos - i;
+                }
+            } else {
+                result[i] = i - pos;
+            }
+        }
+        return result;
+    }
 }
