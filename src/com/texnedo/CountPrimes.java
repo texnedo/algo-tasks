a6a9f12df56552b33ef327755b8a362e6003987e
v.nedashkovskiy <monthtur@gmail.com>
2019-03-31 00:58:19 +0300

    add: addition to the previous commit

--- src/com/texnedo/CountPrimes.java
+++ src/com/texnedo/CountPrimes.java
@@ -1,4 +1,37 @@
 package com.texnedo;
 
 public class CountPrimes {
+    public static void main(String[] args) {
+        CountPrimes primes = new CountPrimes();
+        System.out.println(primes.countPrimes(10));
+    }
+
+    public int countPrimes(int n) {
+        int i = n - 1;
+        int count = 0;
+        while (i > 0) {
+            if (isPrime(i)) {
+                count++;
+            }
+            i--;
+        }
+        return count;
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int maxDivisor = (int)Math.sqrt(n);
+        int i = 2;
+        while (i <= maxDivisor) {
+            System.out.println("n = " + n + "i = " + i);
+            if (n % i == 0) {
+                return false;
+            }
+            i++;
+        }
+        System.out.println("prime = " + n);
+        return true;
+    }
 }
--- src/com/texnedo/MissingNumber.java
+++ src/com/texnedo/MissingNumber.java
@@ -1,4 +1,35 @@
 package com.texnedo;
 
 public class MissingNumber {
+    public static void main(String[] args) {
+        MissingNumber number = new MissingNumber();
+        int[] data = {9,6,4,2,3,5,7,0,1};
+        System.out.println(number.missingNumber(data));
+    }
+
+    public int missingNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            makeVisited(nums, getOriginal(nums[i]));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    private int getOriginal(int i) {
+        if (i >= 0) {
+            return i;
+        }
+        return -i - 1;
+    }
+
+    private void makeVisited(int[] nums, int i) {
+        if (i >= nums.length) {
+            return;
+        }
+        nums[i] = -nums[i] - 1;
+    }
 }
