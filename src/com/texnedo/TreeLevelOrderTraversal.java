3c5fa006bf876223ce910a0ee0646ba19ae1c810
v.nedashkovskiy <monthtur@gmail.com>
2019-05-20 12:21:59 +0300

    add: addition to the previous commit

--- src/com/texnedo/TreeLevelOrderTraversal.java
+++ src/com/texnedo/TreeLevelOrderTraversal.java
@@ -1,4 +1,34 @@
 package com.texnedo;
 
+import com.texnedo.utils.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> result = new LinkedList<>();
+        List<Node> level = new LinkedList<>();
+        List<Node> nextLevel = new LinkedList<>();
+        level.add(root);
+        while (!level.isEmpty()) {
+            List<Integer> levelValues = new ArrayList<>(level.size());
+            for (Node node : level) {
+                levelValues.add(node.val);
+                if (node.children != null) {
+                    nextLevel.addAll(node.children);
+                }
+            }
+            result.add(levelValues);
+            level.clear();
+            level.addAll(nextLevel);
+            nextLevel.clear();
+        }
+        return result;
+    }
 }
--- src/com/texnedo/utils/Node.java
+++ src/com/texnedo/utils/Node.java
@@ -1,4 +1,15 @@
 package com.texnedo.utils;
 
+import java.util.List;
+
 public class Node {
-}
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
