94ce5748b7f42c3f811aef7f0f24998d39638509
v.nedashkovskiy <monthtur@gmail.com>
2020-03-24 00:53:31 +0300

    add: addition to previous commit

--- src/com/texnedo/NumberLongestIncreasingSubsequence.java
+++ src/com/texnedo/NumberLongestIncreasingSubsequence.java
@@ -1,4 +1,19 @@
 package com.texnedo;
 
 public class NumberLongestIncreasingSubsequence {
+    public int findNumberOfLIS(int[] nums) {
+        int[] lengthEndingSubsequence = new int[nums.length];
+        int[] countSubsequence = new int[nums.length];
+        int[] maxItem = new int[nums.length];
+        lengthEndingSubsequence[0] = 1;
+        countSubsequence[0] = 1;
+        maxItem[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                lengthEndingSubsequence[i] = lengthEndingSubsequence[i - 1] + 1;
+                countSubsequence[i] = countSubsequence[i - 1];
+            }
+        }
+        return 0;
+    }
 }
