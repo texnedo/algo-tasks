90f11e33b6fbf9ac6dd4faac2d7c99cb251beae9
Vladimir Nedashkovskiy <monthtur@gmail.com>
2021-11-14 17:54:09 +0300

    add: 1 challenge solved

--- src/com/texnedo/MeetingRooms2.java
+++ src/com/texnedo/MeetingRooms2.java
@@ -14,6 +14,8 @@ public class MeetingRooms2 {
         System.out.println(rooms.minRoomsRequired(
                 Interval.parse("[ [100, 140],  ...  [  0, 30  ], ... [ 20, 40  ]  .., ... [45, 50] ..., [21, 23], [22, 27]]"))
         );
+        int[][] data = {{13,15},{1,13}};
+        System.out.println(rooms.minRoomsRequired2(data));
     }
 
     static class PositionHolder {
@@ -38,6 +40,49 @@ public class MeetingRooms2 {
         Stack<PositionHolder> parallelIntervals = new Stack<>();
         int maxParallel = 0;
         for (PositionHolder holder : holders) {
+            if (holder.isStart) {
+                parallelIntervals.push(holder);
+            } else  {
+                parallelIntervals.pop();
+            }
+            if (maxParallel < parallelIntervals.size()) {
+                maxParallel = parallelIntervals.size();
+            }
+        }
+        return maxParallel;
+    }
+
+    static class PositionHolder2 {
+        final int[] interval;
+        final int point;
+        final boolean isStart;
+
+        PositionHolder2(int[] interval, int point, boolean isStart) {
+            this.interval = interval;
+            this.point = point;
+            this.isStart = isStart;
+        }
+    }
+
+    public int minRoomsRequired2(int[][] intervals) {
+        List<PositionHolder2> holders = new ArrayList<>(intervals.length * 2);
+        for (int i = 0; i < intervals.length; i++) {
+            holders.add(new PositionHolder2(intervals[i], intervals[i][0], true));
+            holders.add(new PositionHolder2(intervals[i], intervals[i][1], false));
+        }
+        holders.sort(new Comparator<PositionHolder2>() {
+            @Override
+            public int compare(PositionHolder2 o1, PositionHolder2 o2) {
+                int result = Integer.compare(o1.point, o2.point);
+                if (result != 0) {
+                    return result;
+                }
+                return Boolean.compare(o1.isStart, o2.isStart);
+            }
+        });
+        Stack<PositionHolder2> parallelIntervals = new Stack<>();
+        int maxParallel = 0;
+        for (PositionHolder2 holder : holders) {
             if (holder.isStart) {
                 parallelIntervals.push(holder);
             } else  {
